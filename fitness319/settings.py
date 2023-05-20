6ca8c06fc25a82c071f414e1f8b637c3d6fa020e
K-Waks <wakskevin@outlook.com>
2023-05-20 20:37:16 +0300

    updated settings.py

--- fitness319/settings.py
+++ fitness319/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from secret import django_secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@c9du*r+o!d99=qzhue))w4=rga9maa@e0o09*monmxi&dxo+#"
+SECRET_KEY = django_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
